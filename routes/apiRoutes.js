fe2e73aa63a768d445641d99a3a991e164e359cc
chevaneze <cpsanon@gmail.com>
2019-03-23 10:39:17 -0400

    ..

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,42 @@
-var db = require("../models");
+var newCharacter = require("../models/character.js");
+
+
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+
+  app.get("/api/all", function(req, res) {
+
+    newCharacter.findAll({}).then(function(results) {
+      res.json(results);
     });
+
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/new", function(req, res) {
+
+    console.log(req.body);
+
+    newCharacter.create({
+        name: req.body.name,
+        class: req.body.class,
+        age: req.body.age,
+        class: req.body.class,
+        height: req.body.height,
+        strength: req.body.strength,
+        weakness: req.body.weakness,
+        description: req.body.description,
+        photo: req.body.photo
+    }).then(function(results) {
+      res.end();
     });
+
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
 };
+//   // Delete an example by id
+//   app.delete("/api/examples/:id", function(req, res) {
+//     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+//       res.json(dbExample);
+//     });
+//   });
+// };
