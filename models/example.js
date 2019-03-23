269ea261fa8b1cf4ae01fbebccca4aeecee3cf21
chevaneze <cpsanon@gmail.com>
2019-03-23 10:51:14 -0400

    ..

--- models/example.js
+++ models/example.js
@@ -1,7 +1,35 @@
-module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
-  });
-  return Example;
-};
+
+var Sequelize = require("sequelize");
+
+var sequelize = require("../config/connection.js");
+
+var newCharacter = sequelize.define("newCharacter", {
+  name: Sequelize.STRING,
+  class: Sequelize.STRING,
+  age: Sequelize.INTEGER,
+  class: Sequelize.STRING,
+  height: Sequelize.STRING,
+  strength: Sequelize.STRING,
+  weakness: Sequelize.STRING,
+  description: Sequelize.STRING,
+  photo: Sequelize.STRING,
+    });
+  
+
+
+newCharacter.sync();
+
+
+module.exports = newCharacter;
+
+
+
+
+
+// module.exports = function(sequelize, DataTypes) {
+//   var Example = sequelize.define("Example", {
+//     text: DataTypes.STRING,
+//     description: DataTypes.TEXT
+//   });
+//   return Example;
+// };
--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,15 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+CREATE DATABASE newCharacter_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+DROP DATABASE IF EXISTS newCharacter_db;
+CREATE DATABASE newCharacter_db;
+
+CREATE TABLE characters(
+name varchar(30) NOT NULL , 
+class  varchar(30) NOT NULL ,
+age  varchar(30)  INTEGER ,
+height  varchar(30) NOT NULL ,
+strength  varchar(30) NOT NULL ,
+weakness  varchar(30) NOT NULL,
+description varchar(30) NOT NULL ,
+photo  varchar(30) NOT NULL ,
+);
