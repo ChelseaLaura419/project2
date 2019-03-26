c472ce93007cba4042d8a7252c8bc3e654247ca5
nBurich <n.burich@hotmail.com>
2019-03-26 19:18:42 -0400

    html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,25 +3,53 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.combat.findAll({}).then(function(dbcombat) {
+    db.combat.findAll({}).then(function(data) {
       res.render("index", {
-        msg: "Welcome!",
-        examples: dbcombat
+        data
       });
     });
   });
 
-//   // Load example page and pass in an example by id
-//   app.get("/example/:id", function(req, res) {
-//     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-//       res.render("example", {
-//         example: dbExample
-//       });
-//     });
-//   });
+  // Load example page and pass in an example by id
+  app.get("/squad", function(req, res) {
+      res.render("squad", {
+        msg: "Welcome"
+      });
+  });
 
+  app.get("/boards", function(req, res) {
+      res.render("boards", {
+        msg: "Welcome"
+      });
+  });
+
+  app.get("/character", function(req, res) {
+      res.render("character", {
+        msg: "Welcome"
+      });
+    });
+
+  app.get("/marvel", function(req, res) {
+      res.render("marvel", {
+        msg: "Welcome"
+      });
+    });
+
+  app.get("/whoniverse", function(req, res) {
+      res.render("whoniverse", {
+        msg: "Welcome"
+      });
+    });
+
+  app.get("/adventure", function(req, res) {
+      res.render("adventure", {
+        msg: "Welcome"
+      });
+  });
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  app.get("404", function(req, res) {
+    res.render("404", {
+      msg: "Welcome"
+    });
   });
 };
