eb0ed2dbdd2283f750bb635e880834b9c55fea4a
nBurich <n.burich@hotmail.com>
2019-03-26 21:02:29 -0400

    user json

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
 
@@ -25,7 +25,7 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: false };
+var syncOptions = { force: true };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
