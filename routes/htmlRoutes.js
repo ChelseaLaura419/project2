28652ccf59d0e0c32284fa847205e1b94f76ba96
Chelsea Laura <cdesrosiers419@gmail.com>
2019-03-26 20:12:00 -0400

    Bootstrap to marvel, more styling

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,6 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-<<<<<<< HEAD
     db.combat.findAll({}).then(function(data) {
       res.render("index", {
         data
@@ -24,29 +23,11 @@ module.exports = function(app) {
         msg: "Welcome"
       });
   });
-=======
-    db.combat.findAll({}).then(function(dbExamples) {
-      res.render("index");
-    });
-  });
 
-  // Load example page and pass in an example by id
-  app.get("/squad", function(req, res) {
-    db.combat.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("squad");
-    });
-  });
-
-  app.get("/boards", function(req, res) {
-    db.combat.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("boards");
-    });
-  });
+  // Load example page and pass in an example by i
 
   app.get("/character", function(req, res) {
-    db.combat.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("character");
-    });
   });
 
   app.get("/marvel", function(req, res) {
@@ -71,47 +52,4 @@ module.exports = function(app) {
     res.render("404");
   });
 };
-//   // Load example page and pass in an example by id
-//   app.get("/example/:id", function(req, res) {
-//     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-//       res.render("example", {
-//         example: dbExample
-//       });
-//     });
-//   });
->>>>>>> b4371cbce27bd0dceb48511d266bbf01bc92077c
 
-  app.get("/character", function(req, res) {
-      res.render("character", {
-        msg: "Welcome"
-      });
-    });
-
-  app.get("/marvel", function(req, res) {
-      res.render("marvel", {
-        msg: "Welcome"
-      });
-    });
-
-  app.get("/whoniverse", function(req, res) {
-      res.render("whoniverse", {
-        msg: "Welcome"
-      });
-    });
-
-  app.get("/adventure", function(req, res) {
-      res.render("adventure", {
-        msg: "Welcome"
-      });
-  });
-  // Render 404 page for any unmatched routes
-<<<<<<< HEAD
-  app.get("404", function(req, res) {
-    res.render("404", {
-      msg: "Welcome"
-    });
-  });
-};
-=======
-
->>>>>>> b4371cbce27bd0dceb48511d266bbf01bc92077c
